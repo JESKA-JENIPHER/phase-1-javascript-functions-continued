76651b8e1c245765f03b880b0a64fa3f9bbd4233
JESKA JENIPHER <jeska.jenipher@student.moringaschool.com>
2022-06-07 15:48:07 +0300

    complete

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // code your solution here
+function saturdayFun(text ="roller-skate") {
+  return `This Saturday, I want to ${text}!`
+}
+saturdayFun();
+
+function mondayWork(text1 = "go to the office"){ 
+  return `This Monday, I will ${text1}.`
+}
+mondayWork("This Monday, I will work from home.");
+
+function wrapAdjective (adjective='*'){
+   const innerFunction = function(value){
+       return `You are ${adjective}${value}${adjective}!`
+   }
+return innerFunction
+}
